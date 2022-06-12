4c97f58b8e5d5c92f25f4b7cdcaddd6d349dabb6
Daffa Arviano <daffa.arviano@outlook.com>
2022-06-12 21:26:06 +0700

    admin crud done

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -10,7 +10,41 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-validation-errors :errors="$errors" />
-{{--                    <x-success-message />--}}
+
+
+                    <button class="block text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 mb-2" type="button" data-modal-toggle="popup-modal">
+                        Delete user
+                    </button>
+                    <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full justify-center items-center" aria-hidden="true">
+                        <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
+                                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </button>
+                                <div class="p-6 text-center">
+                                    <svg class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                    <h3 class="mb-3 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this user?</h3>
+                                    <form method="POST" action="{{ route('admin.destroy', $user) }}">
+                                        @method('DELETE')
+                                        @csrf
+                                        {{--                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">--}}
+                                        {{--                            {{ __('Delete') }}--}}
+                                        {{--                        </button>--}}
+                                        <div class="py-2 align-middle inline-block min-w-full">
+
+                                            <x-button-red data-modal-toggle="popup-modal" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                                {{ __('Delete') }}
+                                            </x-button-red>
+                                        </div>
+                                    </form>
+                                    <button data-modal-toggle="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-4 py-2 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
 
                     <form method="POST" action="{{ route('admin.update', $user) }}">
                         @method('PUT')
@@ -37,19 +71,7 @@
                             </x-button>
                         </div>
                     </form>
-                    <form method="POST" action="{{ route('admin.destroy', $user) }}">
-                        @method('DELETE')
-                        @csrf
-{{--                        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">--}}
-{{--                            {{ __('Delete') }}--}}
-{{--                        </button>--}}
-                        <div class="flex items-center justify-end mt-4">
 
-                            <x-button class="ml-3">
-                                {{ __('Delete') }}
-                            </x-button>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
